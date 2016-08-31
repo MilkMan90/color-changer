c3906d0125f6cd1857c3816bc12673b31264ab26
MilkMan90 <mlkaufman747@gmail.com>
2016-08-30 19:51:07 -0600

    added button functions

--- scripts.js
+++ scripts.js
@@ -3,3 +3,17 @@
 function generateRandomColor(){
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
+
+
+$('#random-button').on('click', function() {
+  var randomColor = generateRandomColor();
+
+  $('body').css('background-color', randomColor);
+
+});
+
+$('#reset-button').on('click', function() {
+
+  $('body').css('background-color', 'white');
+
+});
