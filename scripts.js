5b505312d08977b93bcbb963e26d73eaca6ee9e3
MilkMan90 <mlkaufman747@gmail.com>
2016-09-06 13:32:17 -0600

    fixed button text spacing

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-
 function generateRandomColor(){
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
 $('#random-button').on('click', function() {
   var randomColor = generateRandomColor();
+  while (randomColor === "#FFFFFF") {
+    randomColor = generateRandomColor();
+  }
   $('body').css('background-color', randomColor);
 });
 
